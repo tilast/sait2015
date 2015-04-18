61dec7d7d990d4a94958e289a1b1b3af50c42068
Vitaly Mykhalko <mikhalko.ukr@gmail.com>
2015-04-18 05:52:24 +0300

    periodic cahce updater created - v1

--- redis_cache_update.py
+++ redis_cache_update.py
@@ -5,6 +5,7 @@ import MySQLdb
 import redis
 import simplejson as json
 from datetime import datetime
+import time
 
 DB_HOST = 'localhost'
 DB_USER = 'cache_tester'
@@ -15,6 +16,12 @@ REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 REDIS_DB = 0
 
+
+LIMIT = 50
+NUM_PAGES = 5
+UPDATE_ITERATION_PAUSE = 5  # in seconds
+
+
 QUERY_TEMPLATE = ("SELECT post.id, post.title, post.content, SUM(post_rating.rate) as total_rate "
 					"FROM post "
 					"JOIN post_tag ON post_tag.post_id=post.id "
@@ -23,7 +30,9 @@ QUERY_TEMPLATE = ("SELECT post.id, post.title, post.content, SUM(post_rating.rat
 					"WHERE tag.name='%s' "
 					"GROUP BY post.id "
 					"ORDER BY total_rate DESC "
-					"LIMIT %d;")
+					"LIMIT %d, %d;")
+
+GET_ALL_TAGS_QUERY = ("SELECT name FROM tag;")
 
 #row sequence: id, title, content, total_rate
 def convertRowToPost(row):
@@ -43,45 +52,53 @@ def getMySQLConnection():
 	conn = MySQLdb.connect(host = DB_HOST, user = DB_USER, passwd = DB_PASSWORD, db = DB_NAME)
 	return conn
 
-# def printPost(post):
-# 	print "Post ID: " + str(post["id"])
-# 	print "Title: " + post["title"]
-# 	print "Total Rate: " + str(post["total_rate"])
-# 	print "-----"
-# 	print "Content:"
-# 	print post["content"]
-
 def getRedisConnection():
 	conn = redis.StrictRedis(host = REDIS_HOST, port = REDIS_PORT, db = REDIS_DB)
 	return conn
 
+def getAllTags(conn):
+	cursor = conn.cursor()
+	cursor.execute(GET_ALL_TAGS_QUERY)
+	rows = cursor.fetchall()
+	tags = []
+	for row in rows:
+		tags.append(row[0])
+	return tags
+
 
 def runScript():
-	conn = getMySQLConnection();
-	cursor = conn.cursor()
+	mysqlConn = getMySQLConnection();
+	cursor = mysqlConn.cursor()
 
 	redis = getRedisConnection()
 
-	tag = "iasa"
-	limit = 50
+	while(True):
+		tags = getAllTags(mysqlConn)
+		for tag in tags:
+			for i in range(NUM_PAGES):
+				offset = i * LIMIT
 
-	start_time = datetime.now()
-	cursor.execute(QUERY_TEMPLATE % (tag, limit));
-	
+				start_time = datetime.now()
 
-	redis_query_key = tag + ":" + str(limit) 
+				cursor.execute(QUERY_TEMPLATE % (tag, offset, LIMIT));
+				redis_query_key = tag + ":" + str(offset) + ":" + str(LIMIT)
 
-	posts = []
-	rows = cursor.fetchall()
-	for row in rows:
-		posts.append(convertRowToPost(row))
+				posts = []
+				rows = cursor.fetchall()
+				for row in rows:
+					posts.append(convertRowToPost(row))
+				
+				redis.set(redis_query_key, json.dumps(posts))
 
-	redis.set(redis_query_key, json.dumps(posts))
+				end_time = datetime.now()
+				delta = end_time - start_time
+				output_str = "Query \'" + redis_query_key + "\'" + " was updated. "
+				output_str += "Time elapsed for operation: "
+				output_str += str(delta.total_seconds() * 1000) + "ms"
+				print output_str
 
-	end_time = datetime.now()
-	delta = end_time - start_time
+		time.sleep(UPDATE_ITERATION_PAUSE)
 
-	print "time elapsed for caching query: " + str(delta.total_seconds() * 1000) + "ms"
 
 
 runScript()
